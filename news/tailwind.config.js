6c0fab180ac6659da709e3a12a56436c3c5b672a
DanilSukach <danil211-1@yandex.ru>
2024-05-10 13:37:15 +0400

    добавил navbar

--- news/tailwind.config.js
+++ news/tailwind.config.js
@@ -1,8 +1,20 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: ["./src/**/*.{html,ts}",],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        'Popins': ['Poppins', 'sans-serif'],
+      },
+    },
+    colors: {
+      bg: "#fafafa",
+      white: "#ffffff",
+      black: "#000000",
+      blue: "#1e418d",
+      grey: "#606060",
+      orange: "#FF6633",
+    }
   },
   plugins: [],
 }
