5e07305ad3cab79e75af9e8091ca61f1e0479164
ВЛАДИСЛАВ ВЛАДИСЛАВ <Vladislav290403@gmail.com>
2024-05-10 16:51:50 +0400

    фиговый сервис замутил

--- news/src/app/services/data.service.ts
+++ news/src/app/services/data.service.ts
@@ -1,6 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable, map } from 'rxjs';
+import {Observable, map, BehaviorSubject} from 'rxjs';
 import { NewsInt } from '../interfaces';
 
 @Injectable({
@@ -8,15 +8,27 @@ import { NewsInt } from '../interfaces';
 })
 export class DataService {
   private cardsUrl = 'assets/data.json'; // Путь к вашему JSON-файлу
+   public dataSubject = new BehaviorSubject<NewsInt[]>([]);
+   data$: Observable<NewsInt[]> = this.dataSubject.asObservable();
+  addNews(card: NewsInt) {
 
+    const updatedData = [...this.dataSubject.getValue(), card];
+    console.log(this.data$ );
+    this.dataSubject.next(updatedData);
+    this.data$ = this.dataSubject.asObservable();
+    console.log(this.dataSubject);
+    console.log(this.data$ );
 
+  }
 
   constructor(private http: HttpClient) { }
 
 
   getNews(): Observable<NewsInt[]> {
-    return this.http.get<NewsInt[]>(this.cardsUrl);
-
+     console.log(this.http.get<NewsInt[]>(this.cardsUrl));
+     this.data$ = this.http.get<NewsInt[]>(this.cardsUrl);
+     console.log(this.data$);
+     return this.data$;
   }
   getCardsByNumber(ids: number[]): Observable<NewsInt[]> {
     return this.http.get<NewsInt[]>(this.cardsUrl).pipe(
